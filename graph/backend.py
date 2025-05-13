e5aaa89ebb711a397e240b2bdb2cfe1d39e14368
LuisDa <salazarislas.daniel@gmail.com>
2025-05-13 11:10:58 -0600

    Implementación de dijkstra

--- graph/backend.py
+++ graph/backend.py
@@ -8,42 +8,48 @@ class graph:
     
 
     def __init__(self):
+        source=0
         INF=1e16
         #Se crea la lista de nodos
         list_nodes=[]
         #Se le asigna un id a cada nodo
-        node_id={}
+        id={}
         #Se crea una lista de adyacencia
         adj=[]
         #Se crea un vector para guardar las distancias mínimas
         dist=[]
         #Se crea un vector para guardar los caminos
         path=[]
-
-        #Inicializacion
         #Inicializacion de lista de nodos y id
         list_nodes=dg.generate_nodes()
         for i in range(len(list_nodes)):
-            self.node_id[list_nodes[i]]=i
+            self.id[list_nodes[i]]=i
         #Creación de la lista de adyacencia en base al id
-        adj=[[] for _ in self.node_id]
+        adj=[[] for _ in self.id]
         for a,b,c in dg.generate_edges():
             adj[a].append((c,b))
-        #Inicialización del vector de distancias
+        #Inicialización de los vectores
         self.clean()
-        #Inicialización del vector de caminos
 
-    #Vector dist
     def clean(self):
-        self.dist=[self.INF for _ in self.node_id]
-        self.path=[0 for _ in self.node_id]
+        self.dist=[self.INF for _ in self.id]
+        self.path=[0 for _ in self.id]
 
-    
+    def query_distance(self,node):
+        return self.dist[node]
+
+    def query_path(self,node):
+        stack=[]
+        while self.path[node]!=-1:
+            stack.push(node)
+            node=self.path[node]
+        return stack
+
     def dijkstra(self,s):
-        self.dist[s]=0
-        self.path[s]=0
+        self.dist[self.id[s]]=0
+        self.path[self.id[s]]=-1
         priority_queue=[]
-        heapq.heappush(priority_queue,(0,s))
+        heapq.heappush(priority_queue,(0,self.id[s]))
         while priority_queue:
             current_distance,current_node=heapq.heappop(priority_queue)
             if current_distance>self.dist[current_node]:
@@ -53,6 +59,11 @@ class graph:
                     self.dist[adjnode]=current_distance+weight
                     heapq.heappush(priority_queue,(self.dist[adjnode],adjnode))
 
+    def change_source(self,s):
+        self.clean()
+        self.dijkstra(s)
+
+    
     
 
 
