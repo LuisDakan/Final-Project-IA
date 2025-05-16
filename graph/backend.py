b68c8cfd77733cd3e43b570ebe7d303803c8d782
LuisDa <salazarislas.daniel@gmail.com>
2025-05-15 22:17:06 -0600

    Ya dibuja el camino

--- graph/backend.py
+++ graph/backend.py
@@ -18,14 +18,18 @@ class Graph:
         self.dist=[]
         #Se crea un vector para guardar los caminos
         self.path=[]
+        #Se crea un vector para los caminos con aristas
+        self.path_edge=[]
         #Inicializacion de lista de nodos y id
         self.list_nodes=list(self.geo.generate_nodes())
         for i in range(len(self.list_nodes)):
             self.id[self.list_nodes[i]]=i
+        #Diccionario del id de la arista
         #Creación de la lista de adyacencia en base al id
         self.adj=[[] for _ in self.id]
         for a,b,at in self.geo.generate_edges():
             self.adj[self.id[a]].append((b,float(at["length"])))
+            
         #Inicialización de los vectores
         self.clean()
         #Vector de colores de los nodos
@@ -36,16 +40,21 @@ class Graph:
     def clean(self):
         self.dist=[self.INF for _ in self.id]
         self.path=[0 for _ in self.id]
+        self.path_edge=[0 for _ in self.id]
 
-    def query_distance(self,node):
-        return self.dist[node]
+    def query_distance(self,direction):
+        node=self.geo.get_Node(direction=direction)
+        return self.dist[self.id[node]]
 
-    def query_path(self,node):
-        stack=[]
-        while self.path[node]!=-1:
-            stack.push(node)
-            node=self.path[node]
-        return stack
+    def query_path(self,direction):
+        node=self.geo.get_Node(direction=direction)
+        stack_node=[]
+        stack_edge=[]
+        while self.path[self.id[node]]!=-1:
+            stack_node.append(node)
+            stack_edge.append(self.path_edge[self.id[node]])
+            node=self.path[self.id[node]]
+        return stack_node,stack_edge
 
     def dijkstra(self,s):
         self.dist[self.id[s]]=0
@@ -60,6 +69,8 @@ class Graph:
             for adjnode,weight in self.adj[id_cur]:
                 id_adj=self.id[adjnode]
                 if(current_distance+weight<self.dist[id_adj]):
+                    self.path_edge[id_adj]=(current_node,adjnode)
+                    self.path[id_adj]=current_node
                     self.dist[id_adj]=current_distance+weight
                     heapq.heappush(priority_queue,(self.dist[id_adj],adjnode))
 
@@ -68,10 +79,10 @@ class Graph:
         direction=f"{direction}, Coyoacán, Ciudad de México, México"
         self.source=self.geo.get_Node(direction)
         for i in range(len(self.list_nodes)):
-            if(self.list_nodes[i] in self.geo.general_rest):
-                self.color_nodes[i]='orange'
-            elif(self.list_nodes[i]==self.source):
+            if(self.list_nodes[i]==self.source):
                 self.color_nodes[i]='red'
+            elif(self.list_nodes[i] in self.geo.general_rest):
+                self.color_nodes[i]='orange'
             else:
                 self.color_nodes[i]='blue'
         self.dijkstra(self.source)
@@ -79,15 +90,27 @@ class Graph:
     def print_graph(self):
         ox.plot_graph(self.geo.G,node_color=self.color_nodes,node_size=10)
 
-    def print_path(self):
+    def print_path(self,destiny):
+        path_node,path_edge=self.query_path(destiny)
+        for node in path_node:
+            self.color_nodes[self.id[node]]='red'
+        for i, (u, v, k) in enumerate(self.geo.G.edges(keys=True)):
+            if (u, v) in path_edge or (v, u) in path_edge:
+                self.color_edges[i] = 'red'
+            else:
+                self.color_edges[i]='white'
         ox.plot_graph(self.geo.G,node_color=self.color_nodes,edge_color=self.color_edges,node_size=10)
 
     
         
 if __name__=='__main__':
     grafica=Graph()
-    grafica.set_source("Gamma 32")
-    grafica.print_graph()
+    grafica.set_source("Clavel 68")
+    #grafica.set_source("Museo 150,San Pablo Tepetlapa")
+    #grafica.print_graph()
+    grafica.print_path("Museo 150,San Pablo Tepetlapa")
+
+
     
 
 
